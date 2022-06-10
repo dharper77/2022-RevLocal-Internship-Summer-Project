a932f28921d7e24587acb12961b77b85ba4bdf43
Landon Connell <landonbconnell2001@gmail.com>
2022-06-10 16:35:33 -0400

    mor product css

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -2,7 +2,16 @@ import React from 'react'
 import '../../style/products.css'
 
 const Product = ({ id, title, description, image, price, rating }) => {
-  return <img className="product-preview" key={id} src={image} alt={title} />
+  return (
+    <div className="product-listing">
+      <img src={image} alt={title} />
+      <div>
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <h3>${price}</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Product
