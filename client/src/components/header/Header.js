635a1afa1cdf13b7005a5a4cf9c6a32bfc1da7ff
Landon Connell <landonbconnell2001@gmail.com>
2022-06-15 16:03:42 -0400

    i changed some stuff

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Logo from './Logo'
 import SearchBar from './SearchBar'
 import { Grid } from '@mui/material'
 
 const Header = () => {
   return (
     <Grid container className="header">
-      <Grid item xs={5} sx={{ padding: '0px' }}>
+      {/* logo */}
+      <Grid item xs={4} sx={{ padding: '0px' }}>
+        <Logo />
+      </Grid>
+
+      {/* search bar */}
+      <Grid item xs={4} sx={{ padding: '0px' }}>
         <SearchBar />
       </Grid>
     </Grid>
