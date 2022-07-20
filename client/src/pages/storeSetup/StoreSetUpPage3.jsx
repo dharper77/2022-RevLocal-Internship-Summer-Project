7d3c220934d971d36a14adc635dec1717b4d710b
Landon Connell <lconnell@revlocal.com>
2022-07-20 13:25:21 -0400

    store setup page 3

--- client/src/pages/storeSetup/StoreSetUpPage3.jsx
+++ client/src/pages/storeSetup/StoreSetUpPage3.jsx
@@ -1,4 +1,4 @@
-import { Button, Grid, TextField } from '@mui/material'
+import { Button, Grid, TextField, Divider } from '@mui/material'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Header from '../../components/header/Header'
@@ -7,19 +7,28 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import Stepper from '@mui/material/Stepper'
 import Step from '@mui/material/Step'
 import StepLabel from '@mui/material/StepLabel'
-import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import '../../style/SetUpStore.css'
 
 const StoreSetUpPage3 = () => {
-  const dispatch = useDispatch()
   const [description, setDescription] = useState('')
+  const [logoURL, setLogoURL] = useState('')
+  const [bannerURL, setBannerURL] = useState('')
+  const [state, setState] = useState('')
+  const [city, setCity] = useState('')
   const { shopId } = useParams()
 
   const submitChange = async () => {
     try {
       await axios.patch(`/api/v1/shops/id/${shopId}`, {
-        description
+        description,
+        logoURL,
+        bannerURL,
+        shipsFrom: {
+          city,
+          state
+        }
       })
     } catch (err) {
       console.log(err.message)
@@ -30,23 +39,92 @@ const StoreSetUpPage3 = () => {
     <>
       <Header />
 
-      <Grid container justifyContent="center" sx={{ padding: '0px' }}>
-        <Grid item xs={5}>
-          <h3 style={{ marginTop: '3rem' }}>Shop Description</h3>
+      <Grid container justifyContent="space-evenly" sx={{ paddingTop: '3rem' }}>
+        <Grid
+          container
+          item
+          direction="column"
+          alignItems="flex-start"
+          sx={{
+            padding: '2rem',
+            paddingTop: '0px',
+            border: '1px solid black',
+            borderRadius: '10px'
+          }}
+          xs={6}
+        >
+          <Grid container item justifyContent="center" sx={{ padding: '0px' }}>
+            <Grid item xs={12}>
+              <h2 style={{ marginTop: '2rem' }}>Personalize Shop</h2>
+            </Grid>
+            <Grid item xs={6}>
+              {/* TODO - fix this  */}
+              <Divider style={{ width: '100%' }} />
+            </Grid>
+          </Grid>
+          <Grid item sx={{ padding: '0px' }}>
+            <h3 style={{ marginTop: '2rem' }}>Logo URL:</h3>
+          </Grid>
           <TextField
             fullWidth
+            className="setUpFields"
+            onChange={event => setLogoURL(event.target.value)}
+          />
+          <Grid item sx={{ padding: '0px' }}>
+            <h3 style={{ marginTop: '2rem' }}>Banner URL:</h3>
+          </Grid>
+          <TextField
+            fullWidth
+            className="setUpFields"
+            onChange={event => setBannerURL(event.target.value)}
+          />
+          <Grid item sx={{ padding: '0px' }}>
+            <h3 style={{ marginTop: '2rem' }}>Description:</h3>
+          </Grid>
+          <TextField
+            fullWidth
+            className="description"
             multiline
-            id="description"
-            onChange={event => dispatch(setDescription(event.target.value))}
+            rows={6}
+            onChange={event => setDescription(event.target.value)}
           />
         </Grid>
         <Grid
           container
           item
-          justifyContent="space-between"
+          direction="column"
+          justifyContent="center"
           alignItems="center"
-          sx={{ paddingTop: '18rem' }}
+          sx={{
+            padding: '2rem',
+            paddingTop: '0px',
+            border: '1px solid black',
+            borderRadius: '10px'
+          }}
+          xs={4}
         >
+          <Grid item sx={{ padding: '0px' }}>
+            <h3 style={{ marginTop: '2rem' }}>Where are you shipping from?</h3>
+            <Divider style={{ width: '100%' }} />
+          </Grid>
+          <Grid item sx={{ padding: '0px' }}>
+            <h3 style={{ marginTop: '2rem' }}>State:</h3>
+          </Grid>
+          <TextField
+            fullWidth
+            className="setUpFields"
+            onChange={event => setState(event.target.value)}
+          />
+          <Grid item sx={{ padding: '0px' }}>
+            <h3 style={{ marginTop: '2rem' }}>City:</h3>
+          </Grid>
+          <TextField
+            fullWidth
+            className="setUpFields"
+            onChange={event => setCity(event.target.value)}
+          />
+        </Grid>
+        <Grid container item justifyContent="space-between" alignItems="center">
           <Grid item xs="auto">
             <Link to={`/setUpShop/2/${shopId}`}>
               <Button
